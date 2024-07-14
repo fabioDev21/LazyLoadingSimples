dcb8ab0e2fd4bb78648e6b0a7db40e1132418930
peije21 <fabiotwist1@gmail.com>
2024-07-14 20:16:57 -0300

    adicionado finalização do projeto

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,33 +1,41 @@
 const url = "https://dog.ceo/api/breeds/image/random"
 
-const pegaDados = async () => {
+window.addEventListener("load", () =>{
+    efeitoImagemScroll()
+    pegaDados()
+})
+
+document.querySelector("#attPage").addEventListener("click", () => {
+    window.location.reload()
+})
+
+async function pegaDados(){
     try {
         const imagensCachorros = []
-        for(i = 0; i <= 12; i++){
+        for(i = 0; i < 25; i++){
             const response = await axios.get(url)
-            imagensCachorros.push(response.data)
+            imagensCachorros.push(response.data.message)
         }
-        mostraDados(imagensCachorros)
+        atualizaImagens(imagensCachorros)
     } catch (error) {
         console.log("Deu erro aqui!" + error)
     }
 }
 
-window.addEventListener("load", () => {
-    pegaDados()
-})
-
-function mostraDados(imagensCachorros){
-    const divTotal = document.querySelector("#imagensCachorros")
-    imagensCachorros.forEach(imagem => {
-        const subDiv = document.createElement("div")
-        subDiv.classList.add("subDivImages")
-        subDiv.innerHTML = `<img data-src="${imagem.message}">`
-        divTotal.append(subDiv)
-        efeitoImagemScroll()  
+async function atualizaImagens(imagensCachorros){
+    const totImgs = document.querySelectorAll("img")
+    totImgs.forEach((imagem, i) => {
+        if(i < totImgs.length){
+            imagem.setAttribute("data-src",imagensCachorros[i])
+        }
     })
+    efeitoImagemScroll()
 }
-window.addEventListener("scroll", efeitoImagemScroll)
+
+window.addEventListener("scroll", () => {
+    sobeTopo()
+    efeitoImagemScroll()
+})
 
 function efeitoImagemScroll(){
     document.querySelectorAll('img').forEach(imagem => {
@@ -36,3 +44,17 @@ function efeitoImagemScroll(){
         }
     })
 }
+
+function sobeTopo(){
+    const body = document.querySelector("body")
+    const btnSobeTopo = document.querySelector("#arrowUp")
+    
+    // console.log(body.getBoundingClientRect().top)
+    if(body.getBoundingClientRect().top < -200){
+        btnSobeTopo.style.opacity = 1
+    }
+    if(body.getBoundingClientRect().top > -200){
+        btnSobeTopo.style.opacity = 0
+    }
+}
+
